e7f17dde2edb82dfba1105fc78bb302e92283618
xuxueliang <xu.xueliang@163.com>
2019-05-21 12:28:13 +0800

    edit

--- build/webpack.dev.js
+++ build/webpack.dev.js
@@ -18,7 +18,8 @@ const devWebpackConfig = merge(common, {
     contentBase: './dist', // 告诉服务器从哪提供内容，只有在想要提供静态文件时才需要
     compress: true, // 一切服务都启用gzip 压缩
     host: '0.0.0.0', // 指定使用一个host，可用ip地址访问，没有的话如果别人访问会被禁止。默认localhost。
-    port: '9999', // 指定端口号，如省略，默认为”8080“
+    port: '8080', // 指定端口号，如省略，默认为”8080“
+    disableHostCheck: true,
     hot: true, // 启用模块热替换特性
     inline: true, // 启用内联模式，一段处理实时重载的脚本被插入到bundle中，并且构建消息会出现在浏览器控制台
     quiet: true, // necessary for FriendlyErrorsPlugin
