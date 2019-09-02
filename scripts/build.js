10512597016851009da2a09f8110f24c35b16fb5
xuxueliang <xu.xueliang@163.com>
2019-09-02 19:19:57 +0800

    edit

--- scripts/build.js
+++ scripts/build.js
@@ -2,7 +2,7 @@
  * @Author: xuxueliang
  * @Date: 2019-08-13 19:10:43
  * @LastEditors: xuxueliang
- * @LastEditTime: 2019-08-14 17:55:47
+ * @LastEditTime: 2019-09-02 18:13:39
  */
 const fs = require('fs')
 const path = require('path')
@@ -20,7 +20,9 @@ let builds = require('./config').getAllBuilds()
 if (process.argv[2]) {
   const filters = process.argv[2].split(',')
   builds = builds.filter(b => {
-    return filters.some(f => b.output.file.indexOf(f) > -1 || b._name.indexOf(f) > -1)
+    return filters.some(
+      f => b.output.file.indexOf(f) > -1 || b._name.indexOf(f) > -1
+    )
   })
 } else {
   // filter out weex builds by default
@@ -34,12 +36,20 @@ function build (builds) {
   let built = 0
   const total = builds.length
   const next = () => {
-    buildEntry(builds[built]).then(() => {
-      built++
-      if (built < total) {
-        next()
-      }
-    }).catch(logError)
+    buildEntry(builds[built])
+      .then(() => {
+        built++
+        if (built < total) {
+          next()
+        } else {
+          console.log('----构建完毕----,复制plugins')
+          copyFolder(
+            path.join(__dirname, '../src/lib/plugins'),
+            path.join(__dirname, '../dist/plugins')
+          )
+        }
+      })
+      .catch(logError)
   }
 
   next()
@@ -47,21 +57,24 @@ function build (builds) {
 
 function buildEntry (config) {
   const output = config.output
-  const {file, banner} = output
+  const { file, banner } = output
   const isProd = /(min|prod)\.js$/.test(file)
-  return rollup.rollup(config)
+  return rollup
+    .rollup(config)
     .then(bundle => bundle.generate(output))
-    .then(({output: [{code}]}) => {
+    .then(({ output: [{ code }] }) => {
       if (isProd) {
-        const minified = (banner ? banner + '\n' : '') + terser.minify(code, {
-          toplevel: true,
-          output: {
-            ascii_only: true
-          },
-          compress: {
-            pure_funcs: ['makeMap']
-          }
-        }).code
+        const minified =
+          (banner ? banner + '\n' : '') +
+          terser.minify(code, {
+            toplevel: true,
+            output: {
+              ascii_only: true
+            },
+            compress: {
+              pure_funcs: ['makeMap']
+            }
+          }).code
         return write(file, minified, true)
       } else {
         return write(file, code)
@@ -72,7 +85,12 @@ function buildEntry (config) {
 function write (dest, code, zip) {
   return new Promise((resolve, reject) => {
     function report (extra) {
-      console.log(blue(path.relative(process.cwd(), dest)) + ' ' + getSize(code) + (extra || ''))
+      console.log(
+        blue(path.relative(process.cwd(), dest)) +
+        ' ' +
+        getSize(code) +
+        (extra || '')
+      )
       resolve()
     }
 
@@ -100,4 +118,60 @@ function logError (e) {
 
 function blue (str) {
   return '\x1b[1m\x1b[34m' + str + '\x1b[39m\x1b[22m'
+}
+
+var copyFolder = function (srcDir, tarDir, cb) {
+  fs.readdir(srcDir, function (err, files) {
+    var count = 0
+    var checkEnd = function () {
+      ++count === files.length && cb && cb()
+    }
+
+    if (err) {
+      checkEnd()
+      return
+    }
+
+    files.forEach(function (file) {
+      var srcPath = path.join(srcDir, file)
+      var tarPath = path.join(tarDir, file)
+      fs.stat(srcPath, function (err, stats) {
+        if (stats.isDirectory()) {
+          fs.mkdir(tarPath, function (err) {
+            if (err) {
+              console.log(err)
+              return
+            }
+            copyFolder(srcPath, tarPath, checkEnd)
+          })
+        } else {
+          copyFile(srcPath, tarPath, checkEnd)
+        }
+      })
+    })
+    // 为空时直接回调
+    files.length === 0 && cb && cb()
+  })
+}
+var copyFile = function (srcPath, tarPath, cb) {
+  var rs = fs.createReadStream(srcPath)
+  rs.on('error', function (err) {
+    if (err) {
+      console.log('read error', srcPath)
+    }
+    cb && cb(err)
+  })
+
+  var ws = fs.createWriteStream(tarPath)
+  ws.on('error', function (err) {
+    if (err) {
+      console.log('write error', tarPath)
+    }
+    cb && cb(err)
+  })
+  ws.on('close', function (ex) {
+    cb && cb(ex)
+  })
+
+  rs.pipe(ws)
 }
